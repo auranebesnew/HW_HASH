f864ddea54e61076a4cbd547a18ea98e66eb8a19
ars <ars19990102@gmail.com>
2026-01-09 14:06:55 +0300

    init

--- src/Address.java
+++ src/Address.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class Address {
+    String country;
+    String city;
+
+    public Address(String country, String city) {
+        this.country = country;
+        this.city = city;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country + city);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Address address = (Address) obj;
+        return address.country.equals(this.country)&&address.city.equals(this.city);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how GIGA IDE suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Map<Address, Integer> costPerAddress = new HashMap<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        costPerAddress.put(new Address("Russia", "Moscow"), 1500);
+        costPerAddress.put(new Address("USA", "Denver"), 4000);
+        costPerAddress.put(new Address("China", "Beijing"), 500);
+        costPerAddress.put(new Address("Georgia", "Tbilisi"), 2500);
+        costPerAddress.put(new Address("Poland", "Warsaw"), 2600);
+
+        Scanner scanner = new Scanner(System.in);
+        int cachePrice = 0;
+
+        while (true) {
+            System.out.println("Заполнение нового заказа.");
+            System.out.println("Введите страну: ");
+            String country = scanner.next();
+            System.out.println("Введите город: ");
+            String city = scanner.next();
+            System.out.println("Введите вес (кг): ");
+            String strWeight = scanner.next();
+            int weight = Integer.parseInt(strWeight);
+
+            try {
+                int price = costPerAddress.get(new Address(country, city));
+                int totalPrice = weight * price;
+                cachePrice += totalPrice;
+                System.out.println("Стоимость доставки составит: " + totalPrice + " руб.");
+                System.out.println("Общая стоимость всех доставок: " + cachePrice + " руб.");
+
+            } catch (Exception e) {
+                System.out.println("Доставки по этому адресу нет");
+            }
         }
     }
 }
